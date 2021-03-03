9be95f0ff94128044b46f7c661e54bb274d8e2bf
Vadim Losenkov <vprog0729@gmail.com>
2021-03-03 10:24:08 +0300

    layout ended

--- js/main.js
+++ js/main.js
@@ -14,15 +14,16 @@ $(function () {
   })
 
   const tab = $('.tab')
-
   tab.on('click', function (e) {
     e.preventDefault()
     
     $($(this).siblings()).removeClass('tab--active')
-    $($(this).parent().parent().siblings().find('div')).removeClass('tabs-content--active')
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active')
     
     $(this).addClass('tab--active')
     $($(this).attr('href')).addClass('tabs-content--active')
+    
+    $('.product-slider').slick('setPosition')
 
   })
   const fav = $('.product-item__favorite')
